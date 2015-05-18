4dd3554e913d2fcb248751676acd5051d2d8cca8
babin93 <mababin93@gmail.com>
2015-05-18 13:55:57 -0400

    Project Creation: Hello World

---
+++ src/test/java/SearchUs/client/SandboxGwtTest.java
@@ -0,0 +1,14 @@
+package SearchUs.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class SandboxGwtTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "SearchUs.Project";
+    }
+
+    public void testSandbox() {
+        assertTrue(true);
+    }
+}

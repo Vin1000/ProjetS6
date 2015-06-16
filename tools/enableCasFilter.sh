695de18fde2fbece566abf8cebbe9c79b7512f4d
j3rome <jerome.abdelnour@usherbrooke.ca>
2015-06-16 17:42:04 -0400

    * Fix cas scripts

--- tools/enableCasFilter.sh
+++ tools/enableCasFilter.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 webXml="${DIR}/../src/main/webapp/WEB-INF/web.xml"
 
-sed -i 's/<!-- CAS FILTER START/<!-- CAS FILTER START -->/' ${webXml}
-sed -i 's/CAS FILTER END -->/<!-- CAS FILTER END -->/' ${webXml}
+sed -i 's/<!-- CAS FILTER START/<!-- CAS FILTER START -->/' "${webXml}"
+sed -i 's/CAS FILTER END -->/<!-- CAS FILTER END -->/' "${webXml}"

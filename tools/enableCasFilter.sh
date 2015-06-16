69d691492215a84986d78df1cd13cb31d1375273
j3rome <jerome.abdelnour@usherbrooke.ca>
2015-06-16 17:40:21 -0400

    * Fix CAS scripts

--- tools/enableCasFilter.sh
+++ tools/enableCasFilter.sh
@@ -2,7 +2,7 @@ echo "Enabling CAS Authentication Filter."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-webXml=${DIR}/../src/main/webapp/WEB-INF/web.xml
+webXml="${DIR}/../src/main/webapp/WEB-INF/web.xml"
 
 sed -i 's/<!-- CAS FILTER START/<!-- CAS FILTER START -->/' ${webXml}
 sed -i 's/CAS FILTER END -->/<!-- CAS FILTER END -->/' ${webXml}

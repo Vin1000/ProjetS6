a7c28569499c2292cda727fd1cd581498b660ef0
root <Jerome.abdelnour@usherbrooke.ca>
2015-06-16 17:14:43 -0400

    + Scripts for enabling & disabling CAS filter

--- tools/disableCasFilter.sh
+++ tools/disableCasFilter.sh
@@ -1,10 +1,8 @@
 echo "Disabling CAS Authentication Filter."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-bin=${DIR}/../bin
-lib=${DIR}/../lib
 
 webXml=${DIR}/../src/main/webapp/WEB-INF/web.xml
 
-sed 's/<!-- CAS FILTER START -->/<!-- CAS FILTER START/' ${webXml}
-sed 's/<!-- CAS FILTER END -->/CAS FILTER END -->/' ${webXml}
+sed -i 's/<!-- CAS FILTER START -->/<!-- CAS FILTER START/' ${webXml}
+sed -i 's/<!-- CAS FILTER END -->/CAS FILTER END -->/' ${webXml}
